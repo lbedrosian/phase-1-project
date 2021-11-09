61b7d259a4ece5d39ec0f3ecb27aef4930d202b6
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-11-09 09:06:23 -0500

    almost done

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,7 +1,8 @@
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 2500;	// Time Between Switch 
-// Image List
+// SlideShow
+var i = 0; 			
+var images = [];	
+var time = 2500;	 
+
 images[0] = "images/IMG_0998.jpeg";
 images[1] = "images/IMG_0999.jpeg";
 images[2] = "images/IMG_1006.jpeg";
@@ -9,43 +10,31 @@ images[3] = "images/IMG_1013.jpeg";
 images[4] = "images/IMG_1016.jpeg";
 images[5] = "images/IMG_1020.jpeg";
 images[6] = "images/IMG_1024.jpeg";
-// Change Image
+
 function changeImg(){
 	document.slide.src = images[i];
 	if(i < images.length - 1){
-	  // Add 1 to Index
 	  i++; 
 	} else { 
-		// Reset Back To O
 		i = 0;
 	}
 	setTimeout("changeImg()", time);
 }
 window.onload=changeImg;
-
-fetch('http://api.weatherapi.com/v1/current.json?key=4a4e252153aa48d184e233657210811&q=London&aqi=no')
+//Weather Api
+fetch('http://api.weatherapi.com/v1/current.json?key=4a4e252153aa48d184e233657210811&q=26260&aqi=no')
 .then(Response=>Response.json())
 .then(data=> {
     console.log(data.location)
-    document.querySelector("#weatherApi").innerText = data.object
+    document.querySelector("#weatherApi1").innerText = data.location.name
+    document.querySelector("#weatherApi2").innerText = data.location.region
+    document.querySelector("#weatherApi3").innerText = data.location.country
+    document.querySelector("#weatherApi4").innerText = data.location.localtime
+    document.querySelector("#weatherApi5").innerText = data.current.temp_f
+    document.querySelector("#weatherApi6").innerText = data.current.wind_mph
+    document.querySelector("#weatherApi7").innerText = data.current.humidity
+    document.querySelector("#weatherApi8").innerText = data.current.cloud
+    document.querySelector("#weatherApi9").innerText = data.current.feelslike_f
+
+
 })
-
-
-
-//.then(function (Response) {
-//    return Response.json();
-//})
-//.then(function (data) {
-//    for(var i=0; i<data.length; i++) {
-//        document.getElementById("data").innerHTML +=
-//        data[i].id + "=>" + data[i].firstname + " " + data[i].lastname + "<br /";
-//    }
-//})
-//.catch(function (err) {
- //   console.log(err);
-//});
-
-
-
-//var output = document.getElementById(`output`);
-//output.innerHTML = data;
